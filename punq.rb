559c921382de022a59248f4738c36536ede7135e
punq <punq@mogenius.com>
2023-08-31 18:44:11 +0000

    [skip ci] v1.1.32

--- punq.rb
+++ punq.rb
@@ -4,30 +4,30 @@ class Punq < Formula
   
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/mogenius/homebrew-punq/releases/download/v1.1.31/punq-v1.1.31-darwin-arm64.tar.gz"
-      sha256 "7f56785ba6ec8c7728b9830c3b1fc10c2e0b7c26a634266a5434f57c690dbc25"
+      url "https://github.com/mogenius/homebrew-punq/releases/download/v1.1.32/punq-v1.1.32-darwin-arm64.tar.gz"
+      sha256 "06fe1552b1fab0a60a5050fad6cb8cb7b2163ec13ebd618711580a34a1c7d259"
     elsif Hardware::CPU.intel?
-      url "https://github.com/mogenius/homebrew-punq/releases/download/v1.1.31/punq-v1.1.31-darwin-amd64.tar.gz"
-      sha256 "3670e44335f6e7d34d409a8dd284cdd4a95fb162d30ce4be37e68e8d2196949b"
+      url "https://github.com/mogenius/homebrew-punq/releases/download/v1.1.32/punq-v1.1.32-darwin-amd64.tar.gz"
+      sha256 "b3714be909a9115bc421d50d3fc42fdf06034b4e5a4c6d6c3f266b8b85a6a00c"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/mogenius/homebrew-punq/releases/download/v1.1.31/punq-v1.1.31-linux-amd64.tar.gz"
-        sha256 "b9a8ee5dc0c6d17d90b636e9c4e829b34b5cef08b6cd75d28be7dfaf0884f369"
+        url "https://github.com/mogenius/homebrew-punq/releases/download/v1.1.32/punq-v1.1.32-linux-amd64.tar.gz"
+        sha256 "bb31782b61aea4f3fcfc7da036227bacfa447394a626397a18c1fa205b641cb7"
       else
-        url "https://github.com/mogenius/homebrew-punq/releases/download/v1.1.31/punq-v1.1.31-linux-386.tar.gz"
-        sha256 "abd81d66f9ab5ad504f6aecc178fceb3db1c36ee8fd43abdf2af8d445be37ac7"
+        url "https://github.com/mogenius/homebrew-punq/releases/download/v1.1.32/punq-v1.1.32-linux-386.tar.gz"
+        sha256 "11b67ffccd17f4e586658eef5e8a9d892d71c0b6108ff34d99f3a6847c6b4e1c"
       end
     elif Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/mogenius/homebrew-punq/releases/download/v1.1.31/punq-v1.1.31-linux-arm64.tar.gz"
-        sha256 "167dd51b989cf9e94f69717b6aa963978f6779f77eed5cc7c8255e6932e5aafb"
+        url "https://github.com/mogenius/homebrew-punq/releases/download/v1.1.32/punq-v1.1.32-linux-arm64.tar.gz"
+        sha256 "ed46f878d0959404b38670af5ff63317f9794f6cb8c60ac8340185ee20447186"
       else
-        url "https://github.com/mogenius/homebrew-punq/releases/download/v1.1.31/punq-v1.1.31-linux-arm.tar.gz"
-        sha256 "b44c6a5ab084bf9596f248526d98e8b09be779f8c36b7a6f7968a2bf797f4b50"
+        url "https://github.com/mogenius/homebrew-punq/releases/download/v1.1.32/punq-v1.1.32-linux-arm.tar.gz"
+        sha256 "e5acca136a074bde851df98e584228883275519b0cd3f0718e509cca06168dfa"
       end
     end
   end
@@ -39,27 +39,27 @@ class Punq < Formula
   if OS.mac?
     if Hardware::CPU.arm?
       # Installation steps for macOS ARM64
-      bin.install "punq-v1.1.31-darwin-arm64" => "punq"
+      bin.install "punq-v1.1.32-darwin-arm64" => "punq"
     elsif Hardware::CPU.intel?
       # Installation steps for macOS AMD64
-      bin.install "punq-v1.1.31-darwin-amd64" => "punq"
+      bin.install "punq-v1.1.32-darwin-amd64" => "punq"
     end
   elsif OS.linux?
     if Hardware::CPU.intel?
       if Hardware::CPU.is_64_bit?
         # Installation steps for Linux AMD64
-        bin.install "punq-v1.1.31-linux-amd64" => "punq"
+        bin.install "punq-v1.1.32-linux-amd64" => "punq"
       else
         # Installation steps for Linux 386
-        bin.install "punq-v1.1.31-linux-386" => "punq"
+        bin.install "punq-v1.1.32-linux-386" => "punq"
       end
     elsif Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
         # Installation steps for Linux ARM64
-        bin.install "punq-v1.1.31-linux-arm64" => "punq"
+        bin.install "punq-v1.1.32-linux-arm64" => "punq"
       else
         # Installation steps for Linux ARM
-        bin.install "punq-v1.1.31-linux-arm" => "punq"
+        bin.install "punq-v1.1.32-linux-arm" => "punq"
       end
     end
   end

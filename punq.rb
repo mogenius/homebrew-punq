1400cdab241bf9e9f6b0aa6203092fb48a62e177
punq <punqs@mogenius.com>
2023-09-18 14:55:18 +0000

    [skip ci] v1.2.19

--- punq.rb
+++ punq.rb
@@ -4,62 +4,62 @@ class Punq < Formula
   
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/mogenius/homebrew-punq/releases/download/v1.2.18/punq-v1.2.18-darwin-arm64.tar.gz"
-      sha256 "4f54ad64ce8267aa1b23003389beaccf83539765d2b3f716f7a51406f0c0e591"
+      url "https://github.com/mogenius/homebrew-punq/releases/download/v1.2.19/punq-v1.2.19-darwin-arm64.tar.gz"
+      sha256 "0a2fcf2c0866da49eaecd18db3dc8a9b7f893892b90034fe136b09be30623eac"
     elsif Hardware::CPU.intel?
-      url "https://github.com/mogenius/homebrew-punq/releases/download/v1.2.18/punq-v1.2.18-darwin-amd64.tar.gz"
-      sha256 "5ea4e886caf514e3f28a271fcf725e4398d8c9ac977fd7f99421357a30e8fb62"
+      url "https://github.com/mogenius/homebrew-punq/releases/download/v1.2.19/punq-v1.2.19-darwin-amd64.tar.gz"
+      sha256 "b09e753f4c40e22df60eb52c5b0e1eea17e94df5c66ae5d36959548a65c6dd33"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/mogenius/homebrew-punq/releases/download/v1.2.18/punq-v1.2.18-linux-amd64.tar.gz"
-        sha256 "aa724bd8575918d047a96a7927ec229996119aafe8cad59acdbb5c2a2d938854"
+        url "https://github.com/mogenius/homebrew-punq/releases/download/v1.2.19/punq-v1.2.19-linux-amd64.tar.gz"
+        sha256 "13c747ae60393966ac17b0a6b8563995b55e995c96b910211e1298e2026db64e"
       else
-        url "https://github.com/mogenius/homebrew-punq/releases/download/v1.2.18/punq-v1.2.18-linux-386.tar.gz"
-        sha256 "af2a437f4c463ecb1efa41e5ae580ac2875f2788fd1983f1f43a69da337ce4be"
+        url "https://github.com/mogenius/homebrew-punq/releases/download/v1.2.19/punq-v1.2.19-linux-386.tar.gz"
+        sha256 "dd53eaac705df8566b8be1231ec5d04438ab8e299a072c25acfbe9c3af720e04"
       end
     elif Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/mogenius/homebrew-punq/releases/download/v1.2.18/punq-v1.2.18-linux-arm64.tar.gz"
-        sha256 "4b2e73236ee0ad96e4670681f758714a486275235e69121a9201a67b5f0a45f3"
+        url "https://github.com/mogenius/homebrew-punq/releases/download/v1.2.19/punq-v1.2.19-linux-arm64.tar.gz"
+        sha256 "cb83096b92d575980064439e6772428d53ec25265d2e97b766a2c219472d8c67"
       else
-        url "https://github.com/mogenius/homebrew-punq/releases/download/v1.2.18/punq-v1.2.18-linux-arm.tar.gz"
-        sha256 "73c331ef9c1840e9d8e8014d4eb222ed717b8e1d820b0b2abfe8e0ef300a19fa"
+        url "https://github.com/mogenius/homebrew-punq/releases/download/v1.2.19/punq-v1.2.19-linux-arm.tar.gz"
+        sha256 "ef43f7bb1da2a3b407a9490ddcc80601adcb9c16d7b0e26810e08e0653ef41f6"
       end
     end
   end
   
-  version "1.2.18"
+  version "1.2.19"
   license "MIT"
 
   def install
   if OS.mac?
     if Hardware::CPU.arm?
       # Installation steps for macOS ARM64
-      bin.install "punq-v1.2.18-darwin-arm64" => "punq"
+      bin.install "punq-v1.2.19-darwin-arm64" => "punq"
     elsif Hardware::CPU.intel?
       # Installation steps for macOS AMD64
-      bin.install "punq-v1.2.18-darwin-amd64" => "punq"
+      bin.install "punq-v1.2.19-darwin-amd64" => "punq"
     end
   elsif OS.linux?
     if Hardware::CPU.intel?
       if Hardware::CPU.is_64_bit?
         # Installation steps for Linux AMD64
-        bin.install "punq-v1.2.18-linux-amd64" => "punq"
+        bin.install "punq-v1.2.19-linux-amd64" => "punq"
       else
         # Installation steps for Linux 386
-        bin.install "punq-v1.2.18-linux-386" => "punq"
+        bin.install "punq-v1.2.19-linux-386" => "punq"
       end
     elsif Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
         # Installation steps for Linux ARM64
-        bin.install "punq-v1.2.18-linux-arm64" => "punq"
+        bin.install "punq-v1.2.19-linux-arm64" => "punq"
       else
         # Installation steps for Linux ARM
-        bin.install "punq-v1.2.18-linux-arm" => "punq"
+        bin.install "punq-v1.2.19-linux-arm" => "punq"
       end
     end
   end

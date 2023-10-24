73a1b6350748d2b786a45169ed9f96d6f751c45f
punq <punqs@mogenius.com>
2023-10-24 11:50:26 +0000

    [skip ci] v1.4.10

--- punq.rb
+++ punq.rb
@@ -4,62 +4,62 @@ class Punq < Formula
   
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/mogenius/homebrew-punq/releases/download/v1.4.9/punq-v1.4.9-darwin-arm64.tar.gz"
-      sha256 "a48c38302dedc42e1183d05d3b2b80821a4edb368ecc8990245b5c3395e7d153"
+      url "https://github.com/mogenius/homebrew-punq/releases/download/v1.4.10/punq-v1.4.10-darwin-arm64.tar.gz"
+      sha256 "9efe10384c14a716ad356bc49a70a1580043fc310cbd53a645e6d44e56e80328"
     elsif Hardware::CPU.intel?
-      url "https://github.com/mogenius/homebrew-punq/releases/download/v1.4.9/punq-v1.4.9-darwin-amd64.tar.gz"
-      sha256 "b53a0d1cfb3b44728442aedccc49e934a4e07b1f82bb5c2c93f535248368e6c3"
+      url "https://github.com/mogenius/homebrew-punq/releases/download/v1.4.10/punq-v1.4.10-darwin-amd64.tar.gz"
+      sha256 "d8b567cb879f592d882fc2dd3c2ab03f085aceb3afa3958fc6210c59b1d4643f"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/mogenius/homebrew-punq/releases/download/v1.4.9/punq-v1.4.9-linux-amd64.tar.gz"
-        sha256 "7de3b9cd9445193997dc19db3b6dabe627b6822fa9666276c656922463a06dba"
+        url "https://github.com/mogenius/homebrew-punq/releases/download/v1.4.10/punq-v1.4.10-linux-amd64.tar.gz"
+        sha256 "6da0afb62e2b3093265e6eaedbeba29b63bd3b1a1e12fbd571172a6f5eb72f85"
       else
-        url "https://github.com/mogenius/homebrew-punq/releases/download/v1.4.9/punq-v1.4.9-linux-386.tar.gz"
-        sha256 "6452ed984a1492b29171789c1c5a16ea11bb5974ddf553d708d639be0a4153fe"
+        url "https://github.com/mogenius/homebrew-punq/releases/download/v1.4.10/punq-v1.4.10-linux-386.tar.gz"
+        sha256 "b7e43f3fab4920168a04c412369bf4b1533f59bbea4a97c1b8771ee4a2209389"
       end
     elsif Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/mogenius/homebrew-punq/releases/download/v1.4.9/punq-v1.4.9-linux-arm64.tar.gz"
-        sha256 "4b6c4f38c30167373626927a94eca731af06924141dcc55320cba6812465ef50"
+        url "https://github.com/mogenius/homebrew-punq/releases/download/v1.4.10/punq-v1.4.10-linux-arm64.tar.gz"
+        sha256 "0e3d25a336c86381066092e8c3ef67cd6c2975cb4ccd5f7d7733b13859c77800"
       else
-        url "https://github.com/mogenius/homebrew-punq/releases/download/v1.4.9/punq-v1.4.9-linux-arm.tar.gz"
-        sha256 "5c4372ed0bee9003a0bbd8104b19db6ccb8b8482692fb5c96a2ec3044d4ed0b2"
+        url "https://github.com/mogenius/homebrew-punq/releases/download/v1.4.10/punq-v1.4.10-linux-arm.tar.gz"
+        sha256 "5cfd249af9669cd39986f5af05f2850063704dce461b0dffed8485b2b5b6fead"
       end
     end
   end
   
-  version "1.4.9"
+  version "1.4.10"
   license "MIT"
 
   def install
   if OS.mac?
     if Hardware::CPU.arm?
       # Installation steps for macOS ARM64
-      bin.install "punq-v1.4.9-darwin-arm64" => "punq"
+      bin.install "punq-v1.4.10-darwin-arm64" => "punq"
     elsif Hardware::CPU.intel?
       # Installation steps for macOS AMD64
-      bin.install "punq-v1.4.9-darwin-amd64" => "punq"
+      bin.install "punq-v1.4.10-darwin-amd64" => "punq"
     end
   elsif OS.linux?
     if Hardware::CPU.intel?
       if Hardware::CPU.is_64_bit?
         # Installation steps for Linux AMD64
-        bin.install "punq-v1.4.9-linux-amd64" => "punq"
+        bin.install "punq-v1.4.10-linux-amd64" => "punq"
       else
         # Installation steps for Linux 386
-        bin.install "punq-v1.4.9-linux-386" => "punq"
+        bin.install "punq-v1.4.10-linux-386" => "punq"
       end
     elsif Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
         # Installation steps for Linux ARM64
-        bin.install "punq-v1.4.9-linux-arm64" => "punq"
+        bin.install "punq-v1.4.10-linux-arm64" => "punq"
       else
         # Installation steps for Linux ARM
-        bin.install "punq-v1.4.9-linux-arm" => "punq"
+        bin.install "punq-v1.4.10-linux-arm" => "punq"
       end
     end
   end
